c68c9c6951b5cd5360b4bef9f92519acacabc688
MarenWoodruff <wren403@gmail.com>
2017-06-01 12:00:58 -0400

    updated happy app

--- public/js/ratingsController.js
+++ public/js/ratingsController.js
@@ -1,26 +1,15 @@
 angular
   .module('HappyApp')
   .controller('RatingsController', RatingsController);
-  
+
 function RatingsController() {
   this.all = [1, 0, -1];
-  this.addRating = addRating;
   this.newRating;
-  this.average = averageRating(this.all);
-  function addRating() {
+
+  this.addRating = function addRating() {
     if (this.newRating) {
       this.all.push(parseInt(this.newRating));
     }
-    this.average = averageRating(this.all);
     this.newRating = null;
-  }
-  function averageRating(arr) {
-    var average = 0;
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    average = sum / arr.length;
-    return average;
   }
 }
